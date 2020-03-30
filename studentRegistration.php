72682ffd654661def920db9affdf64d565f14060
Chathura Wanniarachchi <wachathuraj@gmail.com>
2020-03-30 19:56:24 +0530

    Add some styles to the page

--- studentRegistration.php
+++ studentRegistration.php
@@ -14,7 +14,7 @@
             $contact=$_POST['tpNo'];
             $address=$_POST['address'];
             $guardian=$_POST['guardian'];
-    
+
             $sqlGetCount="SELECT COUNT(stuID) FROM student;";
             $resultSetGetCount=mysqli_query($conn,$sqlGetCount);
             $count=mysqli_fetch_assoc($resultSetGetCount)['COUNT(stuID)']+1;
@@ -26,68 +26,77 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Registration</title>
-    <link rel="stylesheet" href="assert/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <?php
-            if($connectionState){
-                echo ("<div style='background-color:green;height:40px;position: absolute;'><h5 style='color:white;text-align: center;'>&nbsp;&nbsp;Conndection Sucessful&nbsp;&nbsp;</h5></div>");
-            }else{
-                echo ("<div style='background-color:red;height:40px;position: absolute;'><h5 style='color:white;text-align: center;'>&nbsp;&nbsp;Conndection Faild&nbsp;&nbsp;</h5></div>");
-            }
-        ?>
-        <div class="row">
-        <div class="col-lg-4" style=""></div>
-    <div class="col-lg-4" style="padding: 16px;margin-top: 30px;height: 1500px;">
-    <h1 style="color:blue;">Student Registration</h1>
-    <img src="stuRegistration.png" alt="" width="100%">
-    <form action="#" method="post" class="form-horizontal">
-        <div class="form-group">
-            <label>Full Name:</label>
-            <input type="text" name="name" class="form-control" >    
-        </div>
-        <div class="form-group">
-            <label>NIC Number:</label>
-            <input type="text" name="nic" class="form-control" >
-        </div>
-        <div class="form-group">
-            <label>Gender:</label><br/>
-            <div class="radio-inline">
-                <input type="radio" name="gender" id="" value="M" class="" >&nbsp;&nbsp;&nbsp;Male:
-                <input type="radio" name="gender" id="" value="F" class="" >&nbsp;&nbsp;&nbsp;Female:
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Student Registration</title>
+        <link rel="stylesheet" href="assert/bootstrap.min.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <?php
+                if($connectionState){
+                    echo ("<div style='background-color:green;height:30px;width:250px;position: absolute;border-radius: 10px;'><h5 style='color:white;text-align: center;'>Conndection Sucessful</h5></div>");
+                }else{
+                    echo ("<div style='background-color:red;height:30px;width:250px;position: absolute;border-radius: 10px;'><h5 style='color:white;text-align: center;'>Conndection Faild</h5></div>");
+                }
+                ?>
+            </div>
+            <br/><br/>
+            <div class="row">
+                <div class="col-lg-4" style=""></div>
+                <div class="col-lg-4">
+                    <h1 style="color:blue;">Student Registration</h1>
+                    <img src="stuRegistration.png" alt="" width="100%">
+                    <form action="#" method="post" class="form-horizontal">
+                        <div class="form-group">
+                            <label>Full Name:</label>
+                            <input type="text" name="name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>NIC Number:</label>
+                            <input type="text" name="nic" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Gender:</label>
+                            <br/>
+                            <div class="radio-inline">
+                                <input type="radio" name="gender" id="" value="M" class="">&nbsp;&nbsp;&nbsp;Male:
+                                <input type="radio" name="gender" id="" value="F" class="">&nbsp;&nbsp;&nbsp;Female:
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label>Date of Birth:</label>
+                            <input type="date" name="dob" id="" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Address:</label>
+                            <textarea name="address" id="address" cols="30" rows="3" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Contact Number:</label>
+                            <input type="text" name="tpNo" id="" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>guardian's Name:</label>
+                            <input type="text" name="guardian" id="" class="form-control">
+                        </div>
+                        <br/>
+                        <div align="center">
+                            <input type="submit" name="submit" value="Submit" class="btn btn-success">
+                            <input type="reset" name="cancel" value="Cancel" class="btn btn-danger">
+                        </div>
+                    </form>
+                </div>
+                <div class="col-lg-4" style=""></div>
+            </div>
+            <br/><br/>
+            <div class="row">
+                <div class="col-lg-12" style="background-color:black;height:100px;border-radius: 10px;"><h5 style="color:white;text-align:center;"><br/><i>Class Managment System</i></h5></div>
             </div>
         </div>
-        <div class="form-group">
-            <label>Date of Birth:</label>
-            <input type="date" name="dob" id="" class="form-control" >
-        </div>
-        <div class="form-group">
-            <label>Address:</label>
-            <textarea name="address" id="address" cols="30" rows="3" class="form-control" ></textarea>
-        </div>
-        <div class="form-group">
-            <label>Contact Number:</label>
-            <input type="text" name="tpNo" id="" class="form-control" >
-        </div>
-        <div class="form-group">
-            <label>guardian's Name:</label>
-            <input type="text" name="guardian" id="" class="form-control">
-        </div>
-        <br/>
-        <div align="center">
-            <input type="submit" name="submit" value="Submit" class="btn btn-success">
-            <input type="reset" name="cancel" value="Cancel" class="btn btn-danger">
-        </div>
-    </form>
-    </div>
-    <div class="col-lg-4" style=""></div>
-        </div>
-    </div>
-    <script src="assert/bootstrap.min.js"></script>
-</body>
+        <script src="assert/bootstrap.min.js"></script>
+    </body>
 </html>
